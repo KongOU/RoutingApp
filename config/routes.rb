31f3244c29fa0cea3309fd49c55a5f08c4500f9d
kongou <oukong84@gmail.com>
2019-06-19 15:01:26 +0700

    Add root pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   get 'routing/customize/contact', to: 'pages#contact', as: 'contact' # as: custom path from routing_customize_contact_path to contact_path
   resources :blogs
+
+  root to: 'pages#home'
 end
