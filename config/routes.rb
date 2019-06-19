8f91797b5a6ab0aec528f168554b8394cc000e05
kongou <oukong84@gmail.com>
2019-06-19 16:37:56 +0700

    Created routes posts, controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   get 'routing/customize/contact', to: 'pages#contact', as: 'contact' # as: custom path from routing_customize_contact_path to contact_path
   resources :blogs
+  resources :posts
 
   root to: 'pages#home'
 end
