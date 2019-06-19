f30f9e314393871297059af51474461b021048ae
kongou <oukong84@gmail.com>
2019-06-19 15:35:41 +0700

    Nesting routes namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'dashboard/main'
+    get 'dashboard/user'
+    get 'dashboard/blog'
+  end
+  
   get 'about', to: 'pages#about'
   get 'routing/customize/contact', to: 'pages#contact', as: 'contact' # as: custom path from routing_customize_contact_path to contact_path
   resources :blogs
