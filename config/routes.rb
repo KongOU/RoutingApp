87494ed4ad80cece9596b5346a3a9c8da3666885
kongou <oukong84@gmail.com>
2019-06-19 14:48:02 +0700

    Customized routes page#contact to routing/customize/contact and run db:create & db:migrate

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   get 'about', to: 'pages#about'
-  get 'contact', to: 'pages#contact'
+  get 'routing/customize/contact', to: 'pages#contact'
   resources :blogs
 end
